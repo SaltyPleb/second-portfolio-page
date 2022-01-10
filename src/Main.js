578ab25307d9441702e44a4a2c0eca87ce682729
Yauheni Bulyha <sergun4ik22@gmail.com>
2022-01-10 14:19:23 +0300

    adding message component

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,14 @@
 import "./style/style.css";
 import Navbar from "./components/Navbar";
 import Cat from "./components/cat/Cat";
+import Message from "./components/message/Message";
 
 function Main() {
   return (
     <>
       <Navbar />
       <Cat />
+      <Message />
     </>
   );
 }
---
+++ src/components/message/Message.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./message.css";
+
+const Message = () => {
+  return (
+    <p className="message">
+      Hello there!
+      <br /> Welcome on my second porfolio page
+    </p>
+  );
+};
+
+export default Message;
