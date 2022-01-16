b22ba7396c0e748fb724ec2ee19de57fe58a1fb7
SaltyPleb <sergun4ik22@gmail.com>
2022-01-16 20:50:53 +0300

    [left_win] [styled-com] add progress bar

--- src/components/middleWindow/LeftSection.js
+++ src/components/middleWindow/LeftSection.js
@@ -5,71 +5,44 @@ import en from "../../static/en-white.png";
 import code from "../../static/code.png";
 import db from "../../static/db.png";
 import design from "../../static/design.png";
+import ProgressBarWithBall from "./ProgressBar.component.js";
 
 const leftSection = () => {
+  const skilsValue = [
+    { bgcolor: "#dfa974", completed: 60, icon: en },
+    { bgcolor: "#dfa974", completed: 30, icon: code },
+    { bgcolor: "#dfa974", completed: 53, icon: design },
+    { bgcolor: "#dfa974", completed: 77, icon: db },
+  ];
   return (
     <div className="left_board">
       <div className="top_progress">
         <div className="item">
-          <div className="">
-            <img className="b-icon" src={volumeWhite} alt="" />
-          </div>
-          <div className="progress">
-            <div className="progress-value first"></div>
-            <div className="circle">
-              <div className="sic" />
-            </div>
-          </div>
+          <img className="b-icon" src={volumeWhite} alt="" />
+          <ProgressBarWithBall completed={75} ball={true} bgcolor={"#dfa974"} />
         </div>
         <div className="item">
-          <div className="">
-            <img className="b-icon" src={settings} alt="" />
-          </div>
-          <div className="progress">
-            <div className="progress-value second"></div>
-            <div className="circle">
-              <div className="sic" />
-            </div>
-          </div>
+          <img className="b-icon" src={settings} alt="" />
+          <ProgressBarWithBall completed={50} ball={true} bgcolor={"#dfa974"} />
         </div>
       </div>
+
       <div className="skils">
-        <div className="item">
-          <div className="">
-            <img className="b-icon" src={en} alt="" />
+        {skilsValue.map((item, index) => (
+          <div className="item">
+            <img key={index} className="b-icon" src={item.icon} alt="" />
+            <ProgressBarWithBall
+              completed={item.completed}
+              bgcolor={item.bgcolor}
+            />
           </div>
-          <div className="progress">
-            <div className="progress-value third"></div>
-          </div>
-        </div>
-        <div className="item">
-          <div className="">
-            <img className="b-icon" src={code} alt="" />
-          </div>
-          <div className="progress">
-            <div className="progress-value"></div>
-          </div>
-        </div>
-        <div className="item">
-          <div className="">
-            <img className="b-icon" src={design} alt="" />
-          </div>
-          <div className="progress">
-            <div className="progress-value"></div>
-          </div>
-        </div>
-        <div className="item">
-          <div className="">
-            <img className="b-icon" src={db} alt="" />
-          </div>
-          <div className="progress">
-            <div className="progress-value"></div>
-          </div>
-        </div>
+        ))}
       </div>
       <div className="about_text">
         <div className="text">
-        Node.JS, React, technical specialist, team lead, project lead. Keen on back-end development using, but also love to produce beautiful and good working front-end part:)
+          Node.JS, React, technical specialist, team lead, project lead. Keen on
+          back-end development using, but also love to produce beautiful and
+          good working front-end part:)
         </div>
       </div>
 
---
+++ src/components/middleWindow/ProgressBar.component.js
@@ -0,0 +1,66 @@
+import React from "react";
+import styled, { keyframes } from "styled-components";
+
+const ProgressBar = (props) => {
+  const { bgcolor, completed, ball } = props;
+
+  const ProgressStyle = styled.div`
+    background: rgba(255, 255, 255, 0.1);
+    align-items: center;
+    justify-content: flex-start;
+    border-radius: 100px;
+    position: relative;
+    padding: 0 5px;
+    display: flex;
+    height: 20px;
+    width: 330px;
+  `;
+
+  const fillProgress = keyframes`
+  from {
+    width: 0%;
+  }
+
+  to {
+    width: ${completed}%;
+  }
+`;
+
+  const ProgressValueStyle = styled.div`
+    position: relative;
+    box-shadow: 0 10px 40px -10px #fff;
+    border-radius: 100px;
+    background: ${bgcolor};
+    height: 15px;
+    animation: ${fillProgress} 3s forwards;
+  `;
+
+  const ProgressCircleStyle = styled.div`
+    position: relative;
+    width: 1px;
+  `;
+
+  const SicStyle = styled.div`
+    position: relative;
+    background: white;
+    border-radius: 100px;
+    height: 20px;
+    width: 20px;
+    left: -15px;
+  `;
+
+  return ball ? (
+    <ProgressStyle>
+      <ProgressValueStyle />
+      <ProgressCircleStyle>
+        <SicStyle />
+      </ProgressCircleStyle>
+    </ProgressStyle>
+  ) : (
+    <ProgressStyle>
+      <ProgressValueStyle></ProgressValueStyle>
+    </ProgressStyle>
+  );
+};
+
+export default ProgressBar;
