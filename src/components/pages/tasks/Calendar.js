c3cc9cbfe485c90abcc65228107001eb4ce61c2c
SaltyPleb <sergun4ik22@gmail.com>
2022-02-20 22:55:58 +0300

    [calendar] added custom schedule with cards

--- src/components/pages/tasks/Calendar.js
+++ src/components/pages/tasks/Calendar.js
@@ -2,10 +2,39 @@ import React, { useEffect } from "react";
 import { ReactComponent as Time } from "../../../static/time.svg";
 import { getDates, getTime, today } from "../../../utils/getDates";
 const Calendar = () => {
-  const schedule = {};
-
   const dates = getDates();
   const time = getTime();
+
+  const schedule = [
+    // only for tests, delete later!
+    {
+      type: "Trips",
+      color: "orange",
+      task: "Went toilet",
+      timeStart: time[0],
+      timeEnd: time[2],
+      margin: "margin-0",
+      hours: 2,
+    },
+    {
+      type: "Trips",
+      color: "orange",
+      task: "Went toilet",
+      timeStart: time[0],
+      timeEnd: time[2],
+      margin: "margin-0",
+      hours: 2,
+    },
+    {
+      type: "Trips",
+      color: "orange",
+      task: "Went toilet",
+      timeStart: time[0],
+      timeEnd: time[2],
+      margin: "margin-0",
+      hours: 2,
+    },
+  ];
 
   useEffect(() => {}, []);
 
@@ -31,13 +60,55 @@ const Calendar = () => {
               ))}
             </div>
 
-            <div className="t-wrap">
-                <div className="line"></div>
-                <div className="task">
-                    <div className="task__type">General</div>
-                    <div className="task__name">Read reviews</div>
-                    <div className="task__time">{time[1]} - {time[3]}</div>
+            {/* {schedule.map(
+              (
+                { type, color, task, timeStart, timeEnd, margin, hours },
+                index
+              ) => (
+                <div className={`t-wrap ${color} ${margin} hours-${hours}`}>
+                  <div className="line"></div>
+                  <div className="task">
+                    <div className="task__type">Trips</div>
+                    <div className="task__name">Went toilet</div>
+                    <div className="task__time">
+                      {time[0]} - {time[2]}
+                    </div>
+                  </div>
                 </div>
+              )
+            )} */}
+
+            <div className="t-wrap orange margin-0 hours-2">
+              <div className="line"></div>
+              <div className="task">
+                <div className="task__type">Trips</div>
+                <div className="task__name">Went toilet</div>
+                <div className="task__time">
+                  {time[0]} - {time[2]}
+                </div>
+              </div>
+            </div>
+
+            <div className="t-wrap green left-2 margin-1 hours-1">
+              <div className="line "></div>
+              <div className="task">
+                <div className="task__type">General</div>
+                <div className="task__name">Read reviews</div>
+                <div className="task__time">
+                  {time[1]} - {time[2]}
+                </div>
+              </div>
+            </div>
+
+            <div className="t-wrap blue left-1 margin-4 hours-2">
+              <div className="line "></div>
+              <div className="task">
+                <div className="task__type">Meeting</div>
+                <div className="task__name">Prepare documents</div>
+                <div className="task__time">
+                  {time[4]} - {time[6]}
+                </div>
+              </div>
             </div>
           </div>
         </div>
