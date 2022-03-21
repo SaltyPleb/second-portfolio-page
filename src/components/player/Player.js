07f38d49663b019c0ffdcad5cd8e6d3a05b76fbe
SaltyPleb <sergun4ik22@gmail.com>
2022-03-21 21:35:42 +0300

    [player draft 2]

--- src/components/player/Player.js
+++ src/components/player/Player.js
@@ -5,13 +5,33 @@ const Player = () => {
   return (
     <div className="player-sp">
       <div className="player">
-        <div className="player__img"></div>
-        <div className="player__btns">
-          <Icon name="icon-heart-o" color="#a5b1c2" size={35}/>
-          <Icon name="icon-link" color="#a5b1c2" size={35}/>
-          <Icon name="icon-prev" color="#a5b1c2" size={35}/>
-          <Icon name="icon-next" color="#a5b1c2" size={35}/>
-          <Icon name="icon-pause" color="#FFFFFF" size={60}/>
+        <div
+          className="player__img"
+          style={{
+            backgroundImage:
+              "url(" +
+              "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/1.jpg" +
+              ")",
+          }}
+        >
+          {/* style="background-image: url("https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/1.jpg");" */}
+        </div>
+        <div className="player-btns">
+          <div className="player-btns__item">
+            <Icon name="icon-heart-o" color="#a5b1c2" size={35} />
+          </div>
+          <div className="player-btns__item">
+            <Icon name="icon-link" color="#a5b1c2" size={35} />
+          </div>
+          <div className="player-btns__item">
+            <Icon name="icon-prev" color="#a5b1c2" size={35} />
+          </div>
+          <div className="player-btns__item">
+            <Icon name="icon-next" color="#a5b1c2" size={35} />
+          </div>
+          <div className="player-btns__item -xl">
+            <Icon name="icon-pause" color="#FFFFFF" size={95} />
+          </div>
         </div>
       </div>
       <div className="control">
